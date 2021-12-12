af4a2300c89ad8a00b15fb367c28b8b7891e524d
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2021-12-12 10:27:22 +0530

    Create LCS_Recursion.cpp

---
+++ LCS_Recursion.cpp
@@ -0,0 +1,23 @@
+//recursive method 
+#include <bits/stdc++.h>
+
+using namespace std;
+int LCS(string x,string y,int n,int m)
+{
+    if(n==0||m==0)
+    return 0;
+    if(x[n-1]==y[m-1])
+    return (1+LCS(x,y,n-1,m-1));
+    else
+    return max(LCS(x,y,n,m-1),LCS(x,y,n-1,m));
+}
+int main()
+{
+    string x,y;
+    cin>>x>>y;
+    int n= x.size();
+    int m= y.size();
+    //cout<<m<<" "<<n;
+    cout<<LCS(x,y,n,m);
+    return 0;
+}
